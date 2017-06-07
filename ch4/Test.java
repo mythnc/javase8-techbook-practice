8227b76dfa86c237a6a3e5be34821cc29cc8998f
N.C Wu <mythnc@gmail.com>
2017-06-07 12:58:20 +0800

    Add chapter4

---
+++ ch4/Test.java
@@ -0,0 +1,11 @@
+class Test {
+    public static void main (String[] args) {
+        Integer i1 = 200;
+        Integer i2 = 200;
+        if (i1.equals(i2)) {
+            System.out.println("i1 == i2");
+        } else {
+            System.out.println("i1 != i2");
+        }
+    }
+} 
