552f202166df46b23bb3777bfd8b3bb8c5cd597b
N.C Wu <mythnc@gmail.com>
2017-06-21 14:06:38 +0800

    Add Anonymous example in LambdaExample

--- ch9/LambdaExample.java
+++ ch9/LambdaExample.java
@@ -1,18 +1,35 @@
-interface Spam {
+interface SpamIF {
     void foo();
+}
+
+class Spam {
+    public void foo() {}
 }
 
 class LambdaExample {
     public static void main (String[] args) {
-        Spam t1 = new Spam() {
+        SpamIF t1 = new SpamIF() {
             @Override
             public void foo() {
-                System.out.println("123");
+                System.out.println("t1");
             }
         };
         t1.foo();
 
-        Spam t2 = () -> System.out.println("abc");
+        SpamIF t2 = () -> System.out.println("t2");
         t2.foo();
+
+        Spam t3 = new Spam() {
+            public void foo() {
+                System.out.println("t3");
+            }
+        };
+        t3.foo();
+
+        new Spam() {
+            public void newFoo() {
+                System.out.println("t4");
+            }
+        }.newFoo();
     }
 }
